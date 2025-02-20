fb1dd10eba69c1f0060dd2f511b6c4a144c617b8
pycanis <pycan@proton.me>
2025-02-20 10:10:38 +0100

    Astro build as middleware + standalone server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-
-import node from '@astrojs/node';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
   adapter: node({
-    mode: 'standalone'
+    mode: 'middleware',
   })
 });
