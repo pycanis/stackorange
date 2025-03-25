5586d37830472ea8e6df6f1c26fabb33091ba85e
pycanis <pycan@proton.me>
2025-03-25 21:15:29 +0100

    Prisma logging

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from "../generated/client";
 
 export * from "../generated/client";
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
